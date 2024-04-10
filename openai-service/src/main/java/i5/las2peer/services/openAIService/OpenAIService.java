5b121c7b2dad1172cd4d5574bebdb4d376e23b2a
Yinnii <y.yin94@outlook.de>
2024-04-10 12:34:38 +0200

    update biwibot materials

--- openai-service/src/main/java/i5/las2peer/services/openAIService/OpenAIService.java
+++ openai-service/src/main/java/i5/las2peer/services/openAIService/OpenAIService.java
@@ -614,25 +614,57 @@ public class OpenAIService extends RESTService {
 		ResultSet rs = null;
 		JSONArray jsonArray = new JSONArray();
 		JSONArray interactiveElements = new JSONArray();
-		JSONObject test = new JSONObject();
-		test.put("couseid", "6");
-		test.put("material", "Lecture Material");
-		jsonArray.add(test);
-		test.put("intent", "material lecture material");
-		test.put("label", "Lecture Material");
-		test.put("description", "Lecture Material");
-		test.put("isFile", false);
-		interactiveElements.add(test);
+		JSONObject lecture = new JSONObject();
+		lecture.put("couseid", channel);
+		lecture.put("material", "Lecture Material");
+		jsonArray.add(lecture);
+		lecture.put("intent", "material lecture material");
+		lecture.put("label", "Vorlesungsmaterialien");
+		lecture.put("description", "Vorlesungsmaterialien");
+		lecture.put("isFile", false);
+		interactiveElements.add(lecture);
 
-		JSONObject test2 = new JSONObject();
-		test2.put("couseid", "6");
-		test2.put("material", "All Seminar Material");
-		jsonArray.add(test2);
-		test2.put("intent", "material all seminar material");
-		test2.put("label", "All Seminar Material");
-		test2.put("description", "All Seminar Material");
-		test2.put("isFile", false);
-		interactiveElements.add(test2);
+		JSONObject seminar_all = new JSONObject();
+		seminar_all.put("couseid", channel);
+		seminar_all.put("material", "All Seminar Material");
+		jsonArray.add(seminar_all);
+		seminar_all.put("intent", "material all seminar material");
+		seminar_all.put("label", "Alle Seminar Materialien");
+		seminar_all.put("description", "Alle Seminar Materialien");
+		seminar_all.put("isFile", false);
+		interactiveElements.add(seminar_all);
+
+		JSONObject all = new JSONObject();
+		all.put("couseid", channel);
+		all.put("material", "All Material");
+		jsonArray.add(all);
+		all.put("intent", "material all material");
+		all.put("label", "Alle Materialien");
+		all.put("description", "Alle Materialien");
+		all.put("isFile", false);
+		interactiveElements.add(all);
+
+		JSONObject organizational = new JSONObject();
+		organizational.put("couseid", channel);
+		organizational.put("material", "Organisational Material");
+		jsonArray.add(organizational);
+		organizational.put("intent", "material organisational material");
+		organizational.put("label", "Organisatorische Materialien");
+		organizational.put("description", "Organisatorische Materialien");
+		organizational.put("isFile", false);
+		interactiveElements.add(organizational);
+		int i = 1;
+		while(i <= 12){
+			JSONObject seminar  = new JSONObject();
+			seminar.put("couseid", channel);
+			seminar.put("material", "Seminar " + Integer.toString(i) + " Material");
+			jsonArray.add(seminar);
+			seminar.put("intent", "material seminar " + Integer.toString(i) + " material");
+			seminar.put("label", "Seminar " + Integer.toString(i) + " Materialien");
+			seminar.put("description", "Seminar " + Integer.toString(i) + " Materialien");
+			seminar.put("isFile", false);
+			interactiveElements.add(seminar);
+		}
 
 		// try {
 		// 	conn = dataSource.getConnection();
