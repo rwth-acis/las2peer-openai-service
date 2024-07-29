ef63035f312044458cd245cef5d09d1e29d78dd2
Yinnii <y.yin94@outlook.de>
2024-07-29 14:55:04 +0200

    update application

--- openai-service/src/main/java/services/openAIService/OpenAIServiceApplication.java
+++ openai-service/src/main/java/services/openAIService/OpenAIServiceApplication.java
@@ -1,5 +1,7 @@
 package services.openAIService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +10,18 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class OpenAIServiceApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(OpenAIServiceApplication.class);
+
 	public static void main(String[] args) {
 		System.setProperty("server.servlet.context-path", "/openai");
-		SpringApplication.run(OpenAIServiceApplication.class, args);
+		try {
+			SpringApplication.run(OpenAIServiceApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Application failed to start.", e);
+		}
+		
+		logger.info("Application has started successfully.");	
 	}
 
 }
